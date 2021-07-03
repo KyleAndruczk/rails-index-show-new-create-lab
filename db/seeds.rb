34ff1f2adb6b0a08bec536e4e0be2bb0d40fbc74
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-07-03 15:02:42 -0400

    commit tree 587703985f84cdccf842d89ef871d3d1ce2d4c99

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+i = 1
+10.times do
+    Coupon.create(coupon_code: "code #{i}", store: "store #{i}")
+    i += 1
+end
